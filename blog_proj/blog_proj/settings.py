5a73cf17fed8f937f845c9e9bdb17a0bbca015d3
Robert Hassenmayer <hassenr@protonmail.com>
2023-01-19 16:22:28 -0500

    production setup started

--- blog_proj/blog_proj/settings.py
+++ blog_proj/blog_proj/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["23.239.14.85", "blogpad.com"]
 
 
 # Application definition
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL='static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- blog_proj/blog_proj/wsgi.py
+++ blog_proj/blog_proj/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_proj.settings')
+sys.path.append("/var/www/blog_site")
+sys.path.append("/var/www/blog_site/blog_proj")
 
 application = get_wsgi_application()
